81ab630a785601b11dbefc9386541c25b5695165
Said Mariano <smariano170@gmail.com>
2025-08-05 13:44:41 -0600

    feat: Obtención de ventas por categoria y sub-categoria

--- scripts/KPIs_ventas_rendimiento_financiero.py
+++ scripts/KPIs_ventas_rendimiento_financiero.py
@@ -10,5 +10,9 @@ suma_sales = df_store_sales['Sales'].sum()
 cantidad_registros, columnas = df_store_sales.shape
 ventas_promedio_pedido = suma_sales/cantidad_registros
 
+# Ventas por categoria y sub-categoria
+categoria_subcategoria = df_store_sales.groupby(['Category','Sub-Category'])['Sales'].sum()
+
 print(f'Ventas Totales: ${suma_sales:.2f}')
 print(f'Ventas Promedio por Pedido: ${ventas_promedio_pedido:.2f}')
+print(f'Ventas por Categoria y Sub-Categoria\n{categoria_subcategoria}')
