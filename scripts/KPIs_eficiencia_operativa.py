8c6ea3fa83d4424d639574dd2fa7fd8e732be9a4
Said Mariano <smariano170@gmail.com>
2025-08-05 16:36:01 -0600

    feat: Transformación de fecha a datetime

--- scripts/KPIs_eficiencia_operativa.py
+++ scripts/KPIs_eficiencia_operativa.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 # Carga del conjunto de datos
 df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
+
+# Tranformación fecha de object a datime
+df_store_sales['Order Date'] = pd.to_datetime(df_store_sales['Order Date'], format='%d/%m/%Y')
+df_store_sales['Ship Date'] = pd.to_datetime(df_store_sales['Ship Date'], format='%d/%m/%Y')
