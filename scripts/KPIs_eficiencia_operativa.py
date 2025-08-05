f6a0a387b80feebfff1cb8f2478bdbda3bb057a7
Said Mariano <smariano170@gmail.com>
2025-08-05 17:01:07 -0600

    feat: Tiempo promedio de envio(dias)

--- scripts/KPIs_eficiencia_operativa.py
+++ scripts/KPIs_eficiencia_operativa.py
@@ -6,3 +6,9 @@ df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
 # Tranformación fecha de object a datime
 df_store_sales['Order Date'] = pd.to_datetime(df_store_sales['Order Date'], format='%d/%m/%Y')
 df_store_sales['Ship Date'] = pd.to_datetime(df_store_sales['Ship Date'], format='%d/%m/%Y')
+
+# Tiempoa promedio de envio
+df_store_sales['Difference in Days'] = (df_store_sales['Ship Date'] - df_store_sales['Order Date'])
+promedio_dias = df_store_sales['Difference in Days'].dt.days.mean()
+
+print(f'Tiempo Promedio de envios(días): {promedio_dias:.0f} días')
