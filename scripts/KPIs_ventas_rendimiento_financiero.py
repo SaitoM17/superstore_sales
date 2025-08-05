a7f6dcc2ec74e55f9069b736a3299a3b0c8d94eb
Said Mariano <smariano170@gmail.com>
2025-08-05 13:48:20 -0600

    feat: Obtención de ventas por región

--- scripts/KPIs_ventas_rendimiento_financiero.py
+++ scripts/KPIs_ventas_rendimiento_financiero.py
@@ -13,6 +13,10 @@ ventas_promedio_pedido = suma_sales/cantidad_registros
 # Ventas por categoria y sub-categoria
 categoria_subcategoria = df_store_sales.groupby(['Category','Sub-Category'])['Sales'].sum()
 
+# Ventas por región
+ventas_region = df_store_sales.groupby('Region')['Sales'].sum()
+
 print(f'Ventas Totales: ${suma_sales:.2f}')
 print(f'Ventas Promedio por Pedido: ${ventas_promedio_pedido:.2f}')
 print(f'Ventas por Categoria y Sub-Categoria\n{categoria_subcategoria}')
+print(f'Ventas por Región\n{ventas_region}')
