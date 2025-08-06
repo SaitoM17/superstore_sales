d8bb3ef9ec9e9c32a8a5bcbc7084b188108dd66f
Said Mariano <smariano170@gmail.com>
2025-08-06 10:37:01 -0600

    feat: Ventas por cliente

--- scripts/KPIs_clientes_mercado.py
+++ scripts/KPIs_clientes_mercado.py
@@ -7,4 +7,9 @@ df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
 clientes_unicos = df_store_sales['Customer ID'].unique()
 cantidad_clientes = len(clientes_unicos)
 
-print(f'Cantidad de clientes unicos: {cantidad_clientes}')
+# Ventas por cliente
+clientes_ventas = df_store_sales.groupby(['Customer ID', 'Customer Name'])['Sales'].sum()
+clientes_ventas_ordenado = clientes_ventas.sort_values(ascending=False)
+
+print(f'Cantidad de Clientes Únicos: {cantidad_clientes}')
+print(f'Ventas por Cliente(Top 10)\n{clientes_ventas_ordenado.head(10)}')
