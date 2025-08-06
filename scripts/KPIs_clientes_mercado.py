53eaec3a7395d23d626a1362a34fef7c73457274
Said Mariano <smariano170@gmail.com>
2025-08-06 11:08:57 -0600

    feat: Ventas por segmento de clientes

--- scripts/KPIs_clientes_mercado.py
+++ scripts/KPIs_clientes_mercado.py
@@ -11,5 +11,12 @@ cantidad_clientes = len(clientes_unicos)
 clientes_ventas = df_store_sales.groupby(['Customer ID', 'Customer Name'])['Sales'].sum()
 clientes_ventas_ordenado = clientes_ventas.sort_values(ascending=False)
 
+# Ventas por segmento de clientes
+ventas_segmento_cliente = df_store_sales.groupby(['Segment'])['Sales'].sum()
+
 print(f'Cantidad de Clientes Únicos: {cantidad_clientes}')
 print(f'Ventas por Cliente(Top 10)\n{clientes_ventas_ordenado.head(10)}')
+print(f'Ventas por Segmento de Clientes: ')
+print(f'{"Segment":<10}{"Sales":>20}')
+for segment, sales in ventas_segmento_cliente.items():
+    print(f'{segment:<11}{"$":>7}{sales:>12,.2f}')
