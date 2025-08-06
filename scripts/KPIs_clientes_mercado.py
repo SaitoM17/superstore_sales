f690be9f43282e591db0e838671d1f872a8250f0
Said Mariano <smariano170@gmail.com>
2025-08-06 09:51:36 -0600

    feat: Carga de conjunto de datos

--- scripts/KPIs_clientes_mercado.py
+++ scripts/KPIs_clientes_mercado.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 # Carga del conjunto de datos
-df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
+df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv') 
