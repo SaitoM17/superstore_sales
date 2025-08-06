e7f23255b599d3ce80b188b4aabf0926b78be567
Said Mariano <smariano170@gmail.com>
2025-08-06 10:17:13 -0600

    feat: Cantidad de clientes unicos

--- scripts/KPIs_clientes_mercado.py
+++ scripts/KPIs_clientes_mercado.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 # Carga del conjunto de datos
 df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv') 
+
+# Conteo de clientes unicos
+clientes_unicos = df_store_sales['Customer ID'].unique()
+cantidad_clientes = len(clientes_unicos)
+
+print(f'Cantidad de clientes unicos: {cantidad_clientes}')
