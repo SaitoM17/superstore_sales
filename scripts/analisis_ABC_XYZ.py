b607ab927910d0841b3dc610673cae3a20c28463
Said Mariano <smariano170@gmail.com>
2025-08-12 13:51:33 -0600

    feat: ventas totales

--- scripts/analisis_ABC_XYZ.py
+++ scripts/analisis_ABC_XYZ.py
@@ -3,4 +3,10 @@ import numpy as np
 
 # Cargar datos
 df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
-print(df_store_sales)
+
+# Calcular ventas totales por productos
+ventas_totales = df_store_sales.groupby('Product Name')['Sales'].sum().reset_index()
+ventas_totales.rename(columns={
+    'Sales': 'Sales_totales'
+}, inplace=True)
+print(ventas_totales)
