3d82a8d0c9e98968f35c095369bd0184beccbf79
Said Mariano <smariano170@gmail.com>
2025-08-12 10:20:24 -0600

    feat: Preparación de los datos

--- scripts/Market_Basket_Analysis.py
+++ scripts/Market_Basket_Analysis.py
@@ -3,3 +3,8 @@ from mlxtend.frequent_patterns import apriori, association_rules
 
 # Carga de datos
 df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
+
+# Preparación de datos
+basket = (df_store_sales.groupby(['Order ID', 'Product Name'])['Product Name']
+          .count().unstack().reset_index().fillna(0)
+          .set_index('Order ID'))
