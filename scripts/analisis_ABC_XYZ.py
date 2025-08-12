3ed859e2621cb2de22d794ef2b67295accc4a54e
Said Mariano <smariano170@gmail.com>
2025-08-12 13:55:37 -0600

    feat: Calculo de porcentaje ABC

--- scripts/analisis_ABC_XYZ.py
+++ scripts/analisis_ABC_XYZ.py
@@ -9,4 +9,9 @@ ventas_totales = df_store_sales.groupby('Product Name')['Sales'].sum().reset_ind
 ventas_totales.rename(columns={
     'Sales': 'Sales_totales'
 }, inplace=True)
+
+# Calcular porcentaje acumulado para ABC
+ventas_totales = ventas_totales.sort_values(by='Sales_totales', ascending=False)
+ventas_totales['Porcentaje'] = (ventas_totales['Sales_totales'] / ventas_totales['Sales_totales'].sum() * 100)
+ventas_totales['Porcentaje_acumulado'] = ventas_totales['Porcentaje'].cumsum()
 print(ventas_totales)
