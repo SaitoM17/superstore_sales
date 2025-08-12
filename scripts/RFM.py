daa73f789938da1a25233fcf44e18d50504cfff8
Said Mariano <smariano170@gmail.com>
2025-08-11 19:01:11 -0600

    feat: Transformación a formato datetime (Order Date y Ship Date)

--- scripts/RFM.py
+++ scripts/RFM.py
@@ -4,3 +4,7 @@ import numpy as np
 # Carga de datos
 df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
 print(df_store_sales.head(5))
+
+# Transformación de Order Date y Ship Date a formato fecha
+df_store_sales['Order Date'] = pd.to_datetime(df_store_sales['Order Date'], format='%d/%m/%Y')
+df_store_sales['Ship Date'] = pd.to_datetime(df_store_sales['Ship Date'], format= '%d/%m/%Y')
