b9178a8b38e2d909a0fd714fe032bf4ac0ca6211
Said Mariano <smariano170@gmail.com>
2025-08-12 11:10:16 -0600

    refactor: Mejoramiento de código

--- scripts/Market_Basket_Analysis.py
+++ scripts/Market_Basket_Analysis.py
@@ -10,13 +10,36 @@ basket = (df_store_sales.groupby(['Order ID', 'Product Name'])['Product Name']
           .set_index('Order ID'))
 
 # Convertir cantidades de 0 a 1
-basket = basket.applymap(lambda x: 1 if x > 0 else 0)
+basket = basket.apply(lambda x: x > 0)
 
 # Aplicar Apriori para encontrar combinaciones frecuentes
-frequent_itemsets = apriori(basket, min_support=0.02, use_colnames=True)
+frequent_itemsets = apriori(basket, min_support=0.005, use_colnames=True)
 
-# Reglas de asociación
-reglas = association_rules(frequent_itemsets, metric='lift', min_threshold=1)
+if frequent_itemsets.empty:
+    print('No se encontraron conjuntos frecuentes')
+else:    
+    # Reglas de asociación
+    reglas = association_rules(frequent_itemsets, metric='lift', min_threshold=1)
 
-# Ordenar por lift más interesante
-reglas = reglas.sort_values(by='lift', ascending=False)
+    # Ordenar por lift más interesante
+    reglas = reglas.sort_values(by='lift', ascending=False)
+
+    # Mostrar resultados
+    print('Combinación Frecuente')
+    print(frequent_itemsets)
+
+    print('\nReglas de Asociación')
+    print(reglas[['antecedents', 'consequents', 'support', 'confidence', 'lift']])
+
+    opcion = int(input(
+        '¿Desea guardar los resultados en archivos CSV?\n'
+        '(1 = Guardar los archivos, cualquier otra tecla = No guardar)\n> '
+    ))
+
+    if opcion == 1:
+        frequent_itemsets.to_csv('./data/processed/frequent_itemsets.csv', index=False)
+        reglas.to_csv('./data/processed/reglas.csv', index=False)
+
+        print('Archivos guardados')
+    else:
+        print('No se guardaron los archivos de clientes VIP y en riesgo.')
