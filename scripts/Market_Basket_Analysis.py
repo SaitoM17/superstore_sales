f6986b754739df48786420fd4ea4e59933a19b6d
Said Mariano <smariano170@gmail.com>
2025-08-12 10:24:06 -0600

    feat: Convertir cantidades

--- scripts/Market_Basket_Analysis.py
+++ scripts/Market_Basket_Analysis.py
@@ -8,3 +8,6 @@ df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
 basket = (df_store_sales.groupby(['Order ID', 'Product Name'])['Product Name']
           .count().unstack().reset_index().fillna(0)
           .set_index('Order ID'))
+
+# Convertir cantidades de 0 a 1
+basket = basket.applymap(lambda x: 1 if x > 0 else 0)
