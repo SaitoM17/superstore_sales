667f190f9d62a564653d415f302f5ea01493f111
Said Mariano <smariano170@gmail.com>
2025-08-13 17:47:21 -0600

    feat: Guardar resultados

--- models/segementacion_comportamiento.py
+++ models/segementacion_comportamiento.py
@@ -79,3 +79,22 @@ for cluster_id, row in perfil_clusters.iterrows():
         print('Grandes compradores')
     else:
         print('Compradores ocasionales')
+
+# Guardar resultados
+try:
+    opcion = int(input(
+        '¿Desea guardar los resultados en archivos CSV?\n'
+        '(1 = Guardar los archivos, cualquier otra tecla = No guardar)\n> '
+    ))
+
+    if opcion == 1:
+        clientes.to_csv("./data/processed/segmentacion_clientes_sin_descuentos.csv", index=False)
+        print('Archivos guardados')
+    else:
+        print('No se guardaron los archivos de clientes VIP y en riesgo.')
+            
+except ValueError:
+    print('Entrada inválida. Debe ingresar un número (por ejemplo, 1 para guardar).')
+
+except Exception as e:
+    print(f'Ocurrió un error al guardar los archivos: {e}')
