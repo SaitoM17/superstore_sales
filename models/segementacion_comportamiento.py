53f466ec98face1837805f421650d4eb19016321
Said Mariano <smariano170@gmail.com>
2025-08-13 13:58:30 -0600

    feat: Calcular metricas

--- models/segementacion_comportamiento.py
+++ models/segementacion_comportamiento.py
@@ -4,4 +4,21 @@ import pandas as pd
 df_store_sales = pd.read_csv('./data/processed/store_sales_limpio.csv')
 
 # Fecha
-df_store_sales['Order Date'] = pd.to_datetime(df_store_sales['Order Date'], format='%d/%m/%y')
+df_store_sales['Order Date'] = pd.to_datetime(df_store_sales['Order Date'], format='%d/%m/%Y')
+
+# Calcular metricas
+fecha_ultima = df_store_sales['Order Date'].max()
+print(fecha_ultima)
+clientes = df_store_sales.groupby('Customer ID').agg({
+    'Order Date': lambda x: (fecha_ultima - x.max()).days,
+    'Customer ID': 'count',
+    'Sales': 'sum'
+})
+
+clientes.rename(columns={
+    'Order Date': 'Recency',
+    'Customer ID': 'Frecuency',
+    'Sales': 'Monetary'
+}, inplace=True)
+
+print(clientes)
